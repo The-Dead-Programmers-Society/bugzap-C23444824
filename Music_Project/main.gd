a630a277b261f4f2c9cfb178c47da8296182701b
C23444824 <c23444824@mytudublin.ie>
2024-04-25 05:33:20 +0100

    Commit 3 - Added the title of the Project - "The Vehicular Auditorium"
    Added more sound effects.

--- Music_Project/main.gd
+++ Music_Project/main.gd
@@ -1,19 +1,39 @@
 extends Node2D
 
-@onready var Beat:AudioStreamPlayer2D = $Beat
+@onready var CarHorn:AudioStreamPlayer2D = $CarHorn
+@onready var CarStart:AudioStreamPlayer2D = $CarStartUp2
+@onready var CarRev:AudioStreamPlayer2D = $CarRev
+@onready var Crossing:AudioStreamPlayer2D = $StreetCrossing
 
 
 
 func _on_play_beat_pressed():
-	if Beat.playing:
-		Beat.stop()
+	if CarHorn.playing:
+		CarHorn.stop()
 	else:
-		Beat.play()
-
-
-func _on_button_pressed():
-	get_tree().reload_current_scene()
+		CarHorn.play()
 
 
 
 	
+
+
+func _on_car_start_up_pressed():
+	if CarStart.playing:
+		CarStart.stop()
+	else:
+		CarStart.play()
+
+
+func _on_car_rev_2_pressed():
+	if CarRev.playing:
+		CarRev.stop()
+	else:
+		CarRev.play()
+
+
+func _on_crossing_pressed():
+	if Crossing.playing:
+		Crossing.stop()
+	else:
+		Crossing.play()
