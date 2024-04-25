ba2cfa6fcb67ded3305746830aaf1a0572d0b8d7
C23444824 <c23444824@mytudublin.ie>
2024-04-25 06:20:06 +0100

    Commit 4 - Added last few sound effects and added Microphone sprite in front of car

--- Music_Project/main.gd
+++ Music_Project/main.gd
@@ -4,6 +4,11 @@ extends Node2D
 @onready var CarStart:AudioStreamPlayer2D = $CarStartUp2
 @onready var CarRev:AudioStreamPlayer2D = $CarRev
 @onready var Crossing:AudioStreamPlayer2D = $StreetCrossing
+@onready var Truck:AudioStreamPlayer2D = $TruckHorn
+@onready var CarEngine:AudioStreamPlayer2D = $Engine
+@onready var Drift:AudioStreamPlayer2D = $Drift
+@onready var Key:AudioStreamPlayer2D = $CarKey
+@onready var Door:AudioStreamPlayer2D = $CarDoorOpen
 
 
 
@@ -37,3 +42,38 @@ func _on_crossing_pressed():
 		Crossing.stop()
 	else:
 		Crossing.play()
+
+
+func _on_t_horn_pressed():
+	if Truck.playing:
+		Truck.stop()
+	else:
+		Truck.play()
+
+
+func _on_car_engine_pressed():
+	if CarEngine.playing:
+		CarEngine.stop()
+	else:
+		CarEngine.play()
+
+
+func _on_car_drift_pressed():
+	if Drift.playing:
+		Drift.stop()
+	else:
+		Drift.play()
+
+
+func _on_car_key_2_pressed():
+	if Key.playing:
+		Key.stop()
+	else:
+		Key.play()
+
+
+func _on_car_door_pressed():
+	if Door.playing:
+		Door.stop()
+	else:
+		Door.play()
